898a84c1f5a33951b7d9527579a9cbb9f9bffcae
adi-mangento <bgus.ramadhn2@gmail.com>
2025-05-25 13:09:21 +0700

    disini saya belajar cara kerja python yang belum di compile dan sudah di compile

---
+++ Belajar python hari 1/Main.py
@@ -0,0 +1,11 @@
+#disini saya belajar cara kerja program dan bedanya eksekusi file python tanpa dicompile dan tidak
+#file Main.py ini filenya tidak dicompile
+#perbedaan kalau di compile terlebih dahulu eksekusi file pythonnya akan lebih cepat
+import time #library time fungsinya untuk ngukur waktu eksekusi file python
+star_time=time.perf_counter()
+print("Maramumaku")
+print(20000-23123132)
+print(time.perf_counter()-star_time)
+
+# hasil yang gk di compile itu
+# memakan waktu 0.0007463000001735054 detik
